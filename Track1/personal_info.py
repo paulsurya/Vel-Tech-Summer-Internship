616b9934a3c0b4bf26a26ba6789f8d4268e91bdb
Paul Surya <paulsurya11@gmail.com>
2026-06-01 10:53:18 +0530

    update:Added personal_info.py

---
+++ Track1/personal_info.py
@@ -0,0 +1,11 @@
+name = "Paul Surya P"
+age = 19
+college_name = "Vel Tech University"
+branch = "B.Tech Artificial Intelligence & Data Science"
+CGPA = 9.1
+
+format_string = f"My name is {name}. I am {age} studying {branch} at {
+    college_name
+} with {CGPA} CGPA"
+
+print(format_string)
